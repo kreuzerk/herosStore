b5ed35ca6c9cff90052a0d60945c6ba19d565658
kreuzerk <kevin.kreuzer@hotmail.com>
2016-06-13 11:22:38 +0200

    Add some default weapons

--- client/src/app/reducers/weapons.reducer.ts
+++ client/src/app/reducers/weapons.reducer.ts
@@ -1,7 +1,20 @@
+import {WeaponFormComponent} from "../components/weapons/create-weapon/weapon-form/weapon-form.component";
 import {WeaponActions} from "../actions/weapon.actions";
 import {Weapon} from "../model/weapon";
 
-export const weapons = (state: Array<Weapon> = [], {type, payload}) => {
+let createWeapon = (name: string, type: string): Weapon => ({
+  id: WeaponFormComponent.counter++,
+  weaponName: name,
+  weaponType: type
+})
+
+let defaultWeapons = [
+  createWeapon('Badmobil', 'Car'),
+  createWeapon('Gas gun', 'Gun'),
+  createWeapon('Iron Mans Suit', 'Suit')
+]
+
+export const weapons = (state: Array<Weapon> = defaultWeapons, {type, payload}) => {
     switch(type){
       case WeaponActions.ADD_WEAPON.toString():
         return [...state, payload];
