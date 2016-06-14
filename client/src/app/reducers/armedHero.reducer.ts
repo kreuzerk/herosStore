4f5c708274b52a156046ea785e0b84b3a93927b2
kreuzerk <kevin.kreuzer@hotmail.com>
2016-06-14 16:00:56 +0200

    You can now delete armed heroes

--- client/src/app/reducers/armedHero.reducer.ts
+++ client/src/app/reducers/armedHero.reducer.ts
@@ -1,10 +1,24 @@
+import {ArmingComponent} from "../components/arming/arming.component";
 import {ArmedHero} from "../model/armedHero.model";
 import {Actions} from "../actions/actions";
 
-export const armedHero = (state: Array<ArmedHero> = [], {type, payload}) => {
+let createArmedHero = (heroId: number, weaponId: number) => ({
+  id: ArmingComponent.counter++,
+  heroId: heroId,
+  weaponId: weaponId
+})
+
+let armedHeros = [
+  createArmedHero(0,0),
+  createArmedHero(1,1)
+]
+
+export const armedHero = (state: Array<ArmedHero> = armedHeros, {type, payload}) => {
   switch(type){
     case Actions.ADD_ARMED_HERO.toString():
       return [...state, payload];
+    case Actions.DELETE_ARMED_HERO.toString():
+      return state.filter(armedHero => armedHero.id !== payload);
     default:
       return state;
   }
